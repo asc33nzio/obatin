4c5cdb069fda0924609f0b4688e2e712270e139b
Franky Perwira Hellyosa Sibuea <franky.sibuea@shopee.com>
2024-05-02 12:19:18 +0000

    fix (BE): SMTP email content, Endpoint Account verif to FE not BE, added role to jwt auth, delete send forget password email after doctor verify the account

--- backend/util/smtp.go
+++ backend/util/smtp.go
@@ -12,6 +12,7 @@ type EmailParams struct {
 	IsAccepted       bool
 	DefaultPassword  string
 	IsForgotPassword bool
+	IsReverified     bool
 }
 
 type SendEmailItf interface {
@@ -32,18 +33,20 @@ func (se *SendEmail) SendVerificationEmail(params EmailParams) error {
 	auth := smtp.PlainAuth("", se.config.EmailUsernameSender(), se.config.AppMailPassword(), se.config.EmailHostSender())
 	var msg string
 
-	if params.IsAccepted && params.VerificationLink != "" && !params.IsForgotPassword {
+	if params.IsAccepted && params.VerificationLink != "" && !params.IsForgotPassword && !params.IsReverified {
 		msg = constant.GetEmailTemplate(se.config.EmailUsernameSender(), params.ToEmail, params.VerificationLink, "", constant.EmailTypeApprove)
 
-	} else if !params.IsAccepted && params.DefaultPassword == "" && params.VerificationLink != "" && !params.IsForgotPassword {
+	} else if !params.IsAccepted && params.DefaultPassword == "" && params.VerificationLink != "" && !params.IsForgotPassword && !params.IsReverified {
 		msg = constant.GetEmailTemplate(se.config.EmailUsernameSender(), params.ToEmail, params.VerificationLink, "", constant.EmailTypeRejected)
+
+	} else if params.IsReverified {
+		msg = constant.GetEmailTemplate(se.config.EmailUsernameSender(), params.ToEmail, params.VerificationLink, "", constant.EmailReverifyAccount)
 
 	} else if params.IsForgotPassword {
 		msg = constant.GetEmailTemplate(se.config.EmailUsernameSender(), params.ToEmail, params.VerificationLink, "", constant.EmailForgotPassword)
 
 	} else {
 		msg = constant.GetEmailTemplate(se.config.EmailUsernameSender(), params.ToEmail, params.VerificationLink, params.DefaultPassword, constant.EmailTypePasswordTemporary)
-
 	}
 	err := smtp.SendMail(se.config.EmailHostSender()+":"+se.config.EmailPortSender(), auth, se.config.EmailUsernameSender(), []string{params.ToEmail}, []byte(msg))
 	if err != nil {
