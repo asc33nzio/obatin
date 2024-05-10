077bdff86e5e647bca39cd075286a66b60c75584
Naufal Yassar <naufal.yassar@shopee.com>
2024-05-10 02:57:04 +0000

    fix (FE): homepage

--- frontend/src/styles/molecules/Banner.styles.tsx
+++ frontend/src/styles/molecules/Banner.styles.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const BannerContainer = styled.div`
   width: 100%;
   height: 500px;
-  padding-top: 2rem;
   border-radius: 24px;
+  overflow: hidden;
   @media (max-width: 769px) {
     height: 300px;
     width: 300px;
@@ -15,7 +15,6 @@ export const Imagecontainer = styled.img`
   width: 100%;
   height: 500px;
   object-fit: cover;
-  border-radius: 24px;
 
   @media (max-width: 769px) {
     height: 300px;
